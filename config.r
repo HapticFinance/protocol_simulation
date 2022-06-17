bf4f7c2f37f1b00450de3be2ffb1a6d84df2530f
alberthaptic <albert@haptic.finance>
2022-06-16 20:24:07 -0400

    optional full tables in report

--- config.r
+++ config.r
@@ -1,7 +1,8 @@
 # Simulation parameters
 
-n_borrowers = 50        # Number of borrowers
-n_stakers = 20          # Number of stakers
-n_pools = 1             # Number of AMM pools
-liq_aft_comp = 1        # Liquidate after compensation
-staker_fixcratio = 1    # Does staker fix c-ratio?
+n_borrowers = 100        # Number of borrowers
+n_stakers = 50           # Number of stakers
+n_pools = 1              # Number of AMM pools
+liq_aft_comp = 1         # Liquidate after compensation
+staker_fixcratio = 1     # Does staker fix c-ratio?
+print_full_tables = 1    # Add full tables to generated pdf
